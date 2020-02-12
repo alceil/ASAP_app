516ce36d46ca82ebe5b3b922ecb02b96079e9d21
alceil <ashishshajisrampickal@gmail.com>
2020-02-12 21:30:09 +0530

    added stless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,25 @@ import 'package:flutter/material.dart';
 void main()
 {
   runApp(MaterialApp(
-    home:Center(
-      child: ListView(
-        children: <Widget>[
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
-          Text('one'),
+    home:HomePage()
+      )
+    );
 
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Column(
+        children: <Widget>[
+          Text('Hello bitch',style: TextStyle(color: Colors.white),),
+          Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
         ],
       )
-    )
-  ));
+    );
+  }
 }
