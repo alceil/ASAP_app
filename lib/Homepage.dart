74c9c7988d4fe22bb5cded2177fe346191c7bf9e
alceil <ashishshajisrampickal@gmail.com>
2020-02-12 23:03:18 +0530

    used inkwelland list tile

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -25,8 +25,62 @@ class _HomePageState extends State<HomePage> {
               backgroundColor: Colors.grey,
               child: Icon(Icons.people,color: Colors.white,)
             ),
-          ),),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.red,
+          ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('HomePage'),
+              leading: Icon(Icons.home),
+            ),
+          ),
 
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('My account'),
+              leading: Icon(Icons.person),
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('My Orders'),
+              leading: Icon(Icons.shopping_basket),
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('categories'),
+              leading: Icon(Icons.category),
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('Favourites'),
+              leading: Icon(Icons.favorite),
+            ),
+          ),
+          Divider(),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(Icons.settings),
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('About'),
+              leading: Icon(Icons.help),
+            ),
+          ),
 
         ],
       ),
