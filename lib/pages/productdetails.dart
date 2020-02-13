d6615caaf2e723e437183b00013dc4a4b38505aa
alceil <ashishshajisrampickal@gmail.com>
2020-02-13 19:09:36 +0530

    added shopping car and favourite button

--- lib/pages/productdetails.dart
+++ lib/pages/productdetails.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 class ProductDetails extends StatefulWidget {
  final pdt_name;
- final pdt_oldprice;
+ final String  pdt_oldprice;
  final pdt_newprice;
  final pdt_picture;
 ProductDetails({this.pdt_name,this.pdt_newprice,this.pdt_oldprice,this.pdt_picture});
@@ -32,11 +32,91 @@ class _ProductDetailsState extends State<ProductDetails> {
                 child: Image.asset(widget.pdt_picture),
               ),
              footer:Container(
-                color: Colors.white,
-              child:Text('Red Dress')
+                color: Colors.white70,
+              child:ListTile(
+                leading: Text(widget.pdt_name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),),
+                title: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text("\$${widget.pdt_oldprice}"),
+                    ),
+                    Expanded(
+                      child: Text("\$${widget.pdt_newprice}",style: TextStyle(decoration: TextDecoration.lineThrough),),
+                    )
+                  ],
+                ),
+
+              )
             ),
             ),
           ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child:Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text('Size'),
+                      ),
+                      Expanded(
+                          child: Icon(Icons.arrow_drop_down)
+                      )
+                    ],
+                  )
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    child:Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text('Color'),
+                        ),
+                        Expanded(
+                            child: Icon(Icons.arrow_drop_down)
+                        )
+                      ],
+                    )
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    child:Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text('Qty'),
+                        ),
+                        Expanded(
+                            child: Icon(Icons.arrow_drop_down)
+                        )
+                      ],
+                    )
+                ),
+              )
+
+
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(onPressed: (){},
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child:Text('Buy Now'),
+                ),
+              ),
+              IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){}),
+              IconButton(icon: Icon(Icons.favorite), onPressed: (){})
+            ],
+          )
         ],
       ),
     );
