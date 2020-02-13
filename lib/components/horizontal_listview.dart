1e6563c50af079c03ecbf91f0f7cb6e0d3d6a2ea
alceil <ashishshajisrampickal@gmail.com>
2020-02-13 10:49:07 +0530

    used category class

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -4,30 +4,17 @@ class HorizontalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
+      height: 100,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
-          Icon(Icons.help),
+          Category(image_location:'images/cats/tshirt.png',image_caption: 'Tshirt' ,),
+          Category(image_location:'images/cats/dress.png',image_caption: 'Dress' ,),
+          Category(image_location:'images/cats/formal.png',image_caption: 'Formal' ,),
+          Category(image_location:'images/cats/tshirt.png',image_caption: 'Tshirt' ,),
+          Category(image_location:'images/cats/tshirt.png',image_caption: 'Tshirt' ,),
+          Category(image_location:'images/cats/tshirt.png',image_caption: 'Tshirt' ,),
+          Category(image_location:'images/cats/tshirt.png',image_caption: 'Tshirt' ,),
         ],
       ),
     );
@@ -36,18 +23,19 @@ class HorizontalList extends StatelessWidget {
 class Category extends StatelessWidget {
   final String image_location;
   final String image_caption;
-  Category(
-  {
-    this.image_location,this.image_caption
-  }
-      )
+  Category({this.image_location,this.image_caption});
   @override
   Widget build(BuildContext context) {
     return Padding(padding: const EdgeInsets.all(2.0),
       child: InkWell(onTap: (){},
-        child: ListTile(
-          title: Image.asset(image_location),
-          subtitle: Text(image_caption),
+        child: Container(
+          width: 100,
+          child: ListTile(
+            title: Image.asset(image_location),
+            subtitle: Container(
+              alignment: Alignment.topCenter,
+                child: Text(image_caption)),
+          ),
         ),
       ),
 
