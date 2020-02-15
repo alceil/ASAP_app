4cbc9c5cbf04701f7510c80dfeeab8571a20b769
alceil <ashishshajisrampickal@gmail.com>
2020-02-15 12:38:15 +0530

    added listview builder

--- lib/components/cart_products.dart
+++ lib/components/cart_products.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
 class Cart_Products extends StatefulWidget {
-  var Cart_ProductList =[
-    {
-      "name":"Blazer",
-      "picture":"images/products/blazer1.jpeg",
-      "old_price":"100",
-      "price":"50"
-    },
-    {
-      "name":"Reddress",
-      "picture":"images/products/dress1.jpeg",
-      "price":"50"
-    },
-
-  ];
-  @override
+ @override
   _Cart_ProductsState createState() => _Cart_ProductsState();
 }
 
 class _Cart_ProductsState extends State<Cart_Products> {
+  var CPL =[
+    {
+      "name":"Blazer",
+      "picture":"images/products/blazer1.jpeg",
+      "price":"50",
+      "Size" : "M",
+      "Color" : "White",
+      "quantity" : 1
+    },
+    {
+      "name":"Reddress",
+      "picture":"images/products/dress1.jpeg",
+      "price":"50",
+      "Size" : "L",
+      "Color" : "Red",
+      "quantity" : 1
+    },
+
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(itemCount:2,itemBuilder:(context,index){
+      return Single_cartProducts(
+        cart_prod_name: CPL[index]["name"],
+        cart_prod_newprice: CPL[index]["name"],
+        cart_prod_pic: CPL[index]["name"],
+        cart_prod_name: CPL[index]["name"],
+
+      );
+    } );
   }
 }
-class Single_cardProducts extends StatelessWidget {
+class Single_cartProducts extends StatelessWidget {
+  final  cart_prod_name;
+  final  cart_prod_pic;
+  final  cart_prod_newprice;
+  final cart_prod_size;
+  Single_cartProducts({this.cart_prod_name,this.cart_prod_newprice,this.cart_prod_pic,this.cart_prod_size});
   @override
   Widget build(BuildContext context) {
     return Container();
